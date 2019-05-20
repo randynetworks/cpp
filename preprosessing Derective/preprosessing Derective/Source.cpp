b528d07d2683c130e74412017ff783a5295a76c4
randyramadhan1973 <randyramadhan1973@gmail.com>
2019-05-21 06:06:57 +0700

    mendeklarasikan mirko define dan menjelaskan preprosessing derective

---
+++ preprosessing Derective/preprosessing Derective/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // ini adalah preprosessing Derective
+
+//mikro
+#define PI 3.14 // ini hanya menyimpan angka, tapi tidak disimpan di memory
+
+using namespace std;
+
+int main() {
+	
+
+
+	cin.get();
+	return 0;
+}
