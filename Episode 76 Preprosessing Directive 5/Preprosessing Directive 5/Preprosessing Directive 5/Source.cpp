1d78bd6e9a7d8983f0c0499080745a457b0a07a3
randyramadhan1973 <randyramadhan1973@gmail.com>
2019-05-22 16:55:11 +0700

    membuat ifdef agar tidak error

--- Episode 76 Preprosessing Directive 5/Preprosessing Directive 5/Preprosessing Directive 5/Source.cpp
+++ Episode 76 Preprosessing Directive 5/Preprosessing Directive 5/Preprosessing Directive 5/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "foo.h"
+#include "bar.h"
 /*
 // ifdef
 #ifdef FOO
--- Episode 76 Preprosessing Directive 5/Preprosessing Directive 5/Preprosessing Directive 5/bar.h
+++ Episode 76 Preprosessing Directive 5/Preprosessing Directive 5/Preprosessing Directive 5/bar.h
@@ -1,1 +1,8 @@
-#define FOO "Ini adalah FOO dalam bar.h"
+// namun jika menggunakan dua FOO ini tidak akan berjalan #define FOO "Ini adalah FOO dalam bar.h" 
+
+// agar tidak error, harus menggunakan ifdef
+
+#ifdef FOO
+#else
+#define FOO "Ini adalah FOO dalam bar.h
+#endif // FOO
