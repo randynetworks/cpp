74f792c7cb8647448afcb188741cbb3d4d8a1f0a
randyramadhan1973 <randyramadhan1973@gmail.com>
2019-05-22 16:21:27 +0700

    membuat ifdef FOO

---
+++ Episode 76 Preprosessing Directive 5/Preprosessing Directive 5/Preprosessing Directive 5/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define FOO "foo Lama"
+
+// ifdef
+#ifdef FOO
+#define TEST_FOO "FOO ada"
+#else
+#define TEST_FOO "FOO tidak ada"
+#endif // FOO
+
+// ifndef
+
+
+// pragma once
