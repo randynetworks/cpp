8708e15ced53f1ed5a8b3e9b19e4be039642488b
randyramadhan1973 <randyramadhan1973@gmail.com>
2019-05-25 08:29:54 +0700

    membuat pragma once di bersama.h

--- Episode 76 Preprosessing Directive 5/Preprosessing Directive 5/Preprosessing Directive 5/bersama.h
+++ Episode 76 Preprosessing Directive 5/Preprosessing Directive 5/Preprosessing Directive 5/bersama.h
@@ -1,8 +1,13 @@
 // untuk membedakan gard (_)  dan makro,
-#ifndef _BERSAMA
-#define _BERSAMA
+//#ifndef _BERSAMA
+//#define _BERSAMA
 
+#pragma once 
+
+/*jadi jika menggunakan pragma once, 
+dia akan mengetaui bahwa bar.h dan foo.h 
+itu tidak usah membaca lagi bersama.h */
 struct mahasiswa{
 	int NIM;
 };
-#endif
+//#endif
